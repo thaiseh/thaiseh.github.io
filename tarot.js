186fc2c3b60cb5222ea49932678f52338bebf199
thaiseh <63216590+thaiseh@users.noreply.github.com>
2021-04-10 14:17:06 -0700

    Update tarot.js

--- tarot.js
+++ tarot.js
@@ -5,49 +5,49 @@ function randomCard() {
       cardName = "Ace of Wands";
       song = "Breakaway";
       artist = "Kelly Clarkson";
-      songLocation = "/songs/breakaway.mp3";
+      songLocation = "songs/Breaking Free - High School Musical - Lyrics.mp3";
       break;
     case 1:
       cardName = "Queen of Wands";
       song = "Firework";
       artist = "Katy Perry";
-      songLocation = "/songs/firework.mp3";
+      songLocation = "songs/Breaking Free - High School Musical - Lyrics.mp3";
       break;
     case 2:
       cardName = "King of Wands";
       song = "Bet on It";
       artist = "Zac Efron in High School Musical 2";
-      songLocation = "/songs/bet.mp3";
+      songLocation = "songs/Breaking Free - High School Musical - Lyrics.mp3";
       break;
     case 3:
       cardName = "Ace of Cups";
       song = "Start of Something New";
       artist = "Zac Efron and Vanessa Hudgens in High School Musical";
-      songLocation = "/songs/start.mp3";
+      songLocation = "songs/Breaking Free - High School Musical - Lyrics.mp3";
       break;
     case 4:
       cardName = "Two of Cups";
       song = "Breaking Free";
       artist = "Zac Efron and Vanessa Hudgens in High School Musical";
-      songLocation = "/songs/breaking.mp3";
+      songLocation = "songs/Breaking Free - High School Musical - Lyrics.mp3";
       break;
     case 5:
       cardName = "Six of Cups";
       song = "This is Me";
       artist = "Demi Lovato and Joe Jonas in Camp Rock";
-      songLocation = "/songs/thisisme.mp3";
+      songLocation = "songs/Breaking Free - High School Musical - Lyrics.mp3";
       break;
     case 6:
       cardName = "Seven of Cups";
       song = "Call Me Maybe";
       artist = "Carly Rae Jepsen";
-      songLocation = "/songs/call.mp3";
+      songLocation = "songs/Breaking Free - High School Musical - Lyrics.mp3";
       break;
     case 7:
       cardName = "Six of Swords";
       song = "Since You've Been Gone";
       artist = "Kelly Clarkson";
-      songLocation = "/songs/since.mp3";
+      songLocation = "songs/Breaking Free - High School Musical - Lyrics.mp3";
       break;
       
 document.getElementById("output").innerHTML = '<p> Your card is the ' + cardName + '. The song that embodies this card, in my opinion, is ' + song + ' by ' + artist +
